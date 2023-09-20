dc4a809395290ad6f0e5765f9846105e04e12832
22021192-NguyenNgocTung <125429268+22021192-NguyenNgocTung@users.noreply.github.com>
2023-09-20 08:33:05 +0700

    test commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App
     public static void main( String[] args ) throws IOException
     {
         DictionaryCommandline.dictionaryAdvanced();
-
+        //tung test
         try {
             CloseableHttpClient httpClient = HttpClients.createDefault();
             String url = "https://api.dexscreener.com/latest/dex/pairs/ethereum/0x9477460179f0a481c9d30d1af177ee865d54ca49";
