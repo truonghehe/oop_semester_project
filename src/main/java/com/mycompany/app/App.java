5c9d3f2188aa70540d1d6fccb3ce1bf933087158
22021192-NguyenNgocTung <125429268+22021192-NguyenNgocTung@users.noreply.github.com>
2023-09-25 13:15:18 +0700

    update

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,6 @@ public class App
 {
     public static void main( String[] args ) throws IOException
     {
-
         DictionaryManagement.insertFromFile();
         DictionaryCommandline.dictionaryAdvanced();
         try {
